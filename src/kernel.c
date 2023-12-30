46e2916c347751b9e0314c2ccdd192da4523fb03
Matrix Man <132014118+Matrix-Man-MM@users.noreply.github.com>
2023-12-30 11:07:48 -0800

    SYS

--- src/kernel.c
+++ src/kernel.c
@@ -1,4 +1,61 @@
+#include "sys.h"
+
+unsigned char* memcpy(unsigned char* dst, const unsigned char* src, int cnt)
+{
+	int i = 0;
+
+	for (; i < cnt; i++)
+		dst[i] = src[i];
+
+	return dst;
+}
+
+unsigned char* memset(unsigned char* dst, unsigned char val, int cnt)
+{
+	int i = 0;
+
+	for (; i < cnt; i++)
+		dst[i] = val;
+
+	return dst;
+}
+
+unsigned short* memsetw(unsigned short* dst, unsigned short val, int cnt)
+{
+	int i = 0;
+
+	for (; i < cnt; i++)
+		dst[i] = val;
+
+	return dst;
+}
+
+int strlen(const char* str)
+{
+	int i = 0;
+
+	while (str[i] != (char)0)
+		i++;
+
+	return i;
+}
+
+unsigned char inb(unsigned short port)
+{
+	unsigned char ret;
+	asm volatile("inb %1, %0" : "=a"(ret) : "dN"(port));
+	return ret;
+}
+
+void outb(unsigned short port, unsigned char data)
+{
+	asm volatile("outb %1, %0" :: "dN"(port), "a"(data));
+}
+
 int kernel_main() {
+	init_vga();
+	puts("AlphaOS\r\n");
+
 	for (;;);
 	return 0;
 }

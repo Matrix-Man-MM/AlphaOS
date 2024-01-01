2d6dcb9e37afc67fe03f30d4c068ceb4689616d7
Matrix Man <132014118+Matrix-Man-MM@users.noreply.github.com>
2024-01-01 12:33:10 -0800

    IDT

---
+++ src/idt.c
@@ -0,0 +1,39 @@
+#include "sys.h"
+
+struct idt_entry
+{
+	unsigned short base_low;
+	unsigned short sel;
+	unsigned char zero;
+	unsigned char flags;
+	unsigned short base_high;
+} __attribute__((packed));
+
+struct idt_pointer
+{
+	unsigned short limit;
+	unsigned int base;
+} __attribute__((packed));
+
+struct idt_entry idt[256];
+struct idt_pointer idtptr;
+
+extern void load_idt();
+
+void idt_set_gate(unsigned char num, unsigned long base, unsigned short sel, unsigned char flags)
+{
+	idt[num].base_low = (base & 0xFFFF);
+	idt[num].base_high = (base >> 16) & 0xFFFF;
+	idt[num].sel = sel;
+	idt[num].zero = 0;
+	idt[num].flags = flags;
+}
+
+void init_idt()
+{
+	idtptr.limit = (sizeof(struct idt_entry) * 256) - 1;
+	idtptr.base = (unsigned int)&idt;
+	memset((unsigned char*)&idt, 0, sizeof(struct idt_entry) * 256);
+
+	load_idt();
+}

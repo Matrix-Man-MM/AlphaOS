665a2c7dc16597d229a18e995f663d9c935a355c
Matrix Man <132014118+Matrix-Man-MM@users.noreply.github.com>
2024-01-03 16:16:34 -0800

    IRQ

--- src/boot.asm
+++ src/boot.asm
@@ -296,6 +296,145 @@ isr_stub:
 	add esp, 8
 	iret
 
+global irq_0
+global irq_1
+global irq_2
+global irq_3
+global irq_4
+global irq_5
+global irq_6
+global irq_7
+global irq_8
+global irq_9
+global irq_10
+global irq_11
+global irq_12
+global irq_13
+global irq_14
+global irq_15
+
+irq_0:
+	cli
+	push byte 0
+	push byte 32
+	jmp irq_stub
+
+irq_1:
+	cli
+	push byte 0
+	push byte 33
+	jmp irq_stub
+
+irq_2:
+	cli
+	push byte 0
+	push byte 34
+	jmp irq_stub
+
+irq_3:
+	cli
+	push byte 0
+	push byte 35
+	jmp irq_stub
+
+irq_4:
+	cli
+	push byte 0
+	push byte 36
+	jmp irq_stub
+
+irq_5:
+	cli
+	push byte 0
+	push byte 37
+	jmp irq_stub
+
+irq_6:
+	cli
+	push byte 0
+	push byte 38
+	jmp irq_stub
+
+irq_7:
+	cli
+	push byte 0
+	push byte 39
+	jmp irq_stub
+
+irq_8:
+	cli
+	push byte 0
+	push byte 40
+	jmp irq_stub
+
+irq_9:
+	cli
+	push byte 0
+	push byte 41
+	jmp irq_stub
+
+irq_10:
+	cli
+	push byte 0
+	push byte 42
+	jmp irq_stub
+
+irq_11:
+	cli
+	push byte 0
+	push byte 43
+	jmp irq_stub
+
+irq_12:
+	cli
+	push byte 0
+	push byte 44
+	jmp irq_stub
+
+irq_13:
+	cli
+	push byte 0
+	push byte 45
+	jmp irq_stub
+
+irq_14:
+	cli
+	push byte 0
+	push byte 46
+	jmp irq_stub
+
+irq_15:
+	cli
+	push byte 0
+	push byte 47
+	jmp irq_stub
+
+extern handle_irq
+
+irq_stub:
+	pusha
+	push ds
+	push es
+	push fs
+	push gs
+	mov ax, 0x10
+	mov ds, ax
+	mov es, ax
+	mov fs, ax
+	mov gs, ax
+	mov eax, esp
+	push eax
+	mov eax, handle_irq
+	call eax
+	pop eax
+	pop gs
+	pop fs
+	pop es
+	pop ds
+	popa
+	add esp, 8
+	iret
+
 section .bss
 	resb 8192
 sys_stack:

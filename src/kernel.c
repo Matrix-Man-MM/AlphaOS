31ed8e6fdea6110eabe07a6e8e269456ee4db947
Matrix Man <132014118+Matrix-Man-MM@users.noreply.github.com>
2024-01-06 22:32:23 -0800

    Retrieve and print multiboot info

--- src/kernel.c
+++ src/kernel.c
@@ -1,4 +1,5 @@
 #include "sys.h"
+#include "multiboot.h"
 
 unsigned char* memcpy(unsigned char* dst, const unsigned char* src, int cnt)
 {
@@ -52,33 +53,59 @@ void outb(unsigned short port, unsigned char data)
 	asm volatile("outb %1, %0" :: "dN"(port), "a"(data));
 }
 
-int kernel_main() {
+int kernel_main(struct multiboot_t* mb_ptr) {
 	init_vga();
-	puts("Welcome to AlphaOS!\r\n");
-
 	init_gdt();
-	puts("GDT Initialized!\r\n");
-
 	init_idt();
-	puts("IDT Initialized!\r\n");
-
 	init_isrs();
-	puts("ISRs Initialized!\r\n");
-
 	init_irq();
-	puts("IRQ Initialized!\r\n");
-
 	asm volatile("sti");
 	init_timer();
-	puts("TIMER Initialized!\r\n");
-
 	init_keyboard();
-	puts("KEYBOARD Initialized!\r\n");
 
-	char* str = "string";
-	int i = 500;
-	printf("PRINTF %s %d\r\n", str, i);
+	set_text_color(15,0);
+	printf("[%s %s]\r\n", KERNEL_NAME, KERNEL_VERSION);
+	set_text_color(2,0);
 
-	for (;;);
+	printf("MULTIBOOT Information\r\n--------------------------------\r\n");
+	set_text_color(7,0);
+	printf("Flags: %x\t", mb_ptr->flags);
+	printf("Mem Lo: %x\t", mb_ptr->mem_lower);
+	printf("Mem Hi: %x\r\n", mb_ptr->mem_upper);
+	printf("BootDev: %x\t", mb_ptr->boot_device);
+	printf("CmdLine: %x\t", mb_ptr->cmdline);
+	printf("Mods: %x\r\n", mb_ptr->mods_count);
+	printf("Addr: %x\t", mb_ptr->mods_addr);
+	printf("Syms: %x\t", mb_ptr->num);
+	printf("Syms: %x\r\n", mb_ptr->size);
+	printf("Syms: %x\t", mb_ptr->addr);
+	printf("Syms: %x\t", mb_ptr->shndx);
+	printf("MMap: %x\r\n", mb_ptr->mmap_length);
+	printf("Addr: %x\t", mb_ptr->mmap_addr);
+	printf("Drives: %x\t", mb_ptr->drives_length);
+	printf("Addr: %x\r\n", mb_ptr->drives_addr);
+	printf("Config: %x\t", mb_ptr->config_table);
+	printf("Loader: %x\t", mb_ptr->boot_loader_name);
+	printf("APM: %x\r\n", mb_ptr->apm_table);
+	printf("VBE Control: %x\t", mb_ptr->vbe_control_info);
+	printf("VBE Mode Info: %x\t", mb_ptr->vbe_mode_info);
+	printf("VBE Mode: %x\r\n", mb_ptr->vbe_mode);
+	printf("VBE Seg: %x\t", mb_ptr->vbe_interface_seg);
+	printf("VBE Off: %x\t", mb_ptr->vbe_interface_off);
+	printf("VBE Len: %x\r\n", mb_ptr->vbe_interface_len);
+	set_text_color(2,0);
+	printf("--------------------------------\r\nEND MULTIBOOT RAW DATA\r\n");
+	set_text_color(3,0);
+	printf("Started with %s\r\n", (char*)mb_ptr->cmdline);
+	printf("Booted from %s\r\n", (char*)mb_ptr->boot_loader_name);
+
+	reset_text_color();
+	printf("Testing VGA Text Colors...\r\n");
+	for (int i = 0; i < 16; i++)
+	{
+		set_text_color(i,0);
+		putc('A');
+	}
+
 	return 0;
 }

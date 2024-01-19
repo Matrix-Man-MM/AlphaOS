a5325215ae65dcc888f7029a2d937bc15733a3ad
Matrix Man <132014118+Matrix-Man-MM@users.noreply.github.com>
2024-01-19 12:38:52 -0800

    initrd

--- drive/drive.c
+++ drive/drive.c
@@ -1,14 +1,30 @@
 #include <stdio.h>
 #include <string.h>
 
+void create_initrd()
+{
+	char data[1024];
+	FILE* file_ptr;
+	file_ptr = fopen("initrd", "w");
+
+	int i;
+	for (i = 0; i < 512; i++)
+	{
+		memset(data, '\0', sizeof(data));
+		fwrite(data, sizeof(data), 1, file_ptr);
+	}
+
+	fclose(file_ptr);
+}
+
 void create_empty_drive()
 {
-	char data[512];
+	char data[1024];
 	FILE* file_ptr;
 	file_ptr = fopen("drive.hdd", "w");
 
 	int i;
-	for (i = 0; i < 524288; i++)
+	for (i = 0; i < (1024 * 512); i++)
 	{
 		memset(data, '\0', sizeof(data));
 		fwrite(data, sizeof(data), 1, file_ptr);
@@ -19,6 +35,7 @@ void create_empty_drive()
 
 int main()
 {
+	create_initrd();
 	create_empty_drive();
 
 	return 0;

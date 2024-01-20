5145730ceeea4f4ae9dece23bb338b05cf8c73c5
Matrix Man <132014118+Matrix-Man-MM@users.noreply.github.com>
2024-01-20 15:58:27 -0800

    Cleanup

--- drive/drive.c
+++ drive/drive.c
@@ -1,22 +1,6 @@
 #include <stdio.h>
 #include <string.h>
 
-void create_initrd()
-{
-	char data[1024];
-	FILE* file_ptr;
-	file_ptr = fopen("initrd", "w");
-
-	int i;
-	for (i = 0; i < 512; i++)
-	{
-		memset(data, '\0', sizeof(data));
-		fwrite(data, sizeof(data), 1, file_ptr);
-	}
-
-	fclose(file_ptr);
-}
-
 void create_empty_drive()
 {
 	char data[1024];
@@ -35,7 +19,6 @@ void create_empty_drive()
 
 int main()
 {
-	create_initrd();
 	create_empty_drive();
 
 	return 0;

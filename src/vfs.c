fb2e6d28a855453dec3cb5ec08c5a609c774a274
Matrix Man <132014118+Matrix-Man-MM@users.noreply.github.com>
2024-01-25 02:08:35 -0800

    aopen() function implementation

--- src/vfs.c
+++ src/vfs.c
@@ -41,7 +41,7 @@ struct dirent* readdir_vfs(vfs_node_t* node, uint32_t index)
 
 vfs_node_t* finddir_vfs(vfs_node_t* node, char* name)
 {
-	if ((node->flags & 0x07) == VFS_DIRECTORY && node->readdir != 0)
+	if ((node->flags & VFS_DIRECTORY) && node->readdir != 0)
 		return node->finddir(node, name);
 	else
 		return (vfs_node_t*)NULL;
@@ -49,5 +49,48 @@ vfs_node_t* finddir_vfs(vfs_node_t* node, char* name)
 
 vfs_node_t* aopen(const char* filename, uint32_t flags)
 {
+	if (!vfs_root)
+		KERNEL_HALT("Attempted to aopen() without filesystem!");
 
+	if (!filename[0] == '/')
+		KERNEL_HALT("Attempted to aopen() with a non-absolute path!");
+
+	uint32_t path_len = strlen(filename);
+	char* path = (char*)amalloc(sizeof(char) * (path_len));
+	memcpy(path, filename, path_len);
+	char* path_offset = path;
+	uint32_t path_depth = 0;
+
+	while (path_offset < path + path_len)
+	{
+		if (*path_offset == '/')
+		{
+
+			*path_offset = '\0';
+			path_depth++;
+		}
+
+		path_offset++;
+	}
+
+	path[path_len] = '\0';
+	path_offset = path + 1;
+	uint32_t depth;
+	vfs_node_t* node_ptr = vfs_root;
+
+	for (depth = 0; depth < path_depth; ++depth)
+	{
+		node_ptr = finddir_vfs(node_ptr, path_offset);
+
+		if (!node_ptr)
+		{
+			free((void*)path);
+			return NULL;
+		}
+		else if (depth == path_depth - 1)
+			return node_ptr;
+	}
+
+	free((void*)path);
+	return NULL;
 }
